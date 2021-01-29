5bc8e8e4df53cfd1b3f1308467d5ab30b8f3c292
Christoph Grotz <christophgrotz@gmail.com>
2021-01-29 19:46:02 +0100

    Added methods for interoperatability with spot micro kinetics

--- control.py
+++ control.py
@@ -18,6 +18,11 @@ class Joint:
         angle_in_pw = self.center + angle * self.degree_to_pwm_width
         if angle_in_pw > min and angle_in_pw < max:
             self.pwm.set_pwm( self.pwm_id, 0, angle_in_pw )
+
+    def setAngle(self, angle):
+        angle_in_pw = angle * self.degree_to_pwm_width
+        if angle_in_pw > min and angle_in_pw < max:
+            self.pwm.set_pwm( self.pwm_id, 0, angle_in_pw )
         
 class Leg:
     def __init__(self, wrist, hip, shoulder):
@@ -31,6 +36,20 @@ class Leg:
         self.wrist.rest()
         self.hip.rest()
 
+    ''' Sets the angles for this legs.
+
+        Used for interoperability with Spot Micro Python
+        Args:
+            angles: Angles in the order q1,q2,q3. (hip, leg, knee)
+                      An example output:
+                         (lb_q1,lb_q2,lb_q3)
+        Returns:
+            None
+    '''
+    def setAngles(self, angles):
+        self.shoulder.setAngle(angles[0])
+        self.hip.setAngle(angles[1])
+        self.wrist.setAngle(angles[2])
 
 class MotorControl:
     def __init__(self, left_front, left_back, right_front, right_back):
@@ -44,5 +63,26 @@ class MotorControl:
         self.left_back.rest()
         self.right_front.rest()
         self.right_back.rest()
+    
+    ''' Sets the leg angles for all four legs.
+
+        Used for interoperability with Spot Micro Python
+        Args:
+            angles: Tuple of 4 of the leg angles. Legs in the order rightback
+                      rightfront, leftfront, leftback. Angles in the order q1,q2,q3. (hip, leg, knee)
+                      An example output:
+                        ((rb_q1,rb_q2,rb_q3),
+                         (rf_q1,rf_q2,rf_q3),
+                         (lf_q1,lf_q2,lf_q3),
+                         (lb_q1,lb_q2,lb_q3))
+        Returns:
+            None
+    '''
+    def setAngles(self, angles):
+        self.right_back.setAngles(angles[0])
+        self.right_front.setAngles(angles[1])
+        self.left_front.setAngles(angles[2])
+        self.left_back.setAngles(angles[3])
+
 
 
